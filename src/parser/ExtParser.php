3791801b9e956a5ad9a5da278cfb74b632d0c85e
Sphinx <sphinx@sphinx.li>
2017-01-13 02:27:38 +0100

    Add headers with GNU v3 notice

--- src/parser/ExtParser.php
+++ src/parser/ExtParser.php
@@ -1,5 +1,25 @@
 <?php
 
+/**
+ *  This file is part of the PHP-ExtJS Loader Project.
+ *
+ *  PHP-ExtJS is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  PHP-ExtJS is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with PHP-ExtJS.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *  @author: Sphinxila
+ *  @date: 13.01.2017
+ */
+
 namespace ExtJSLoader\Parser;
 
 trait ExtParser {
